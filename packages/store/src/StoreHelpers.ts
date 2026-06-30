d40418fa407070b15cf6b7215650bbf117005c26
Mike Kobit <mkobit@gmail.com>
2026-06-29 19:42:08 -0500

    task(sm-1oge): implement and test removeConstraintForSpace store action

--- packages/store/src/StoreHelpers.ts
+++ packages/store/src/StoreHelpers.ts
@@ -1,6 +1,7 @@
 import { AppState } from "./StoreTypes";
 import { SpaceConstraint } from "@storagemaxxing/assembly/SpaceConstraint";
 import { SpaceTemplateId } from "@storagemaxxing/assembly/SpaceTemplate";
+import { BinSpecId } from "@storagemaxxing/assembly/BaseTypes";
 
 export const updateConstraintInState = (
   state: AppState,
@@ -22,6 +23,30 @@ export const updateConstraintInState = (
           ...s.constraints,
           [constraint.binId]: constraint,
         },
+      };
+    }),
+  };
+};
+
+export const removeConstraintFromState = (
+  state: AppState,
+  templateId: SpaceTemplateId,
+  binId: BinSpecId,
+) => {
+  const existing = state.constraintsBySpace[templateId] || [];
+  const filtered = existing.filter((c) => c.binId !== binId);
+  return {
+    constraintsBySpace: {
+      ...state.constraintsBySpace,
+      [templateId]: filtered,
+    },
+    spaces: state.spaces.map((s) => {
+      if (s.templateId !== templateId) return s;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { [binId]: _, ...newConstraints } = s.constraints;
+      return {
+        ...s,
+        constraints: newConstraints,
       };
     }),
   };
